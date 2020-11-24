9a06746332f58dee2ce61e0011c9b5ab9095c396
hamstar0 <webutek@gmail.com>
2020-11-24 09:44:55 -0800

    v1.2.0.1 - Fixed issue preventing warps in general (without API intervention)

--- WarpItemControlLib/MyPlayer.cs
+++ WarpItemControlLib/MyPlayer.cs
@@ -6,7 +6,7 @@ using HamstarHelpers.Helpers.Debug;
 
 namespace WarpItemControlLib {
 	partial class WICPlayer : ModPlayer {
-		public bool IsWarpEnabled { get; internal set; } = false;
+		public bool IsWarpEnabled { get; internal set; } = true;
 
 		public string WarpDeniedMessage { get; internal set; } = "Warp items disabled.";
 
